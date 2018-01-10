1c7b0d4c38c8fb74f00af3a3d1d7449c65cd3b69
Jibin Mathews <jibinmathews7@gmail.com>
2018-01-10 18:27:15 +0530

    Initial PoC

---
+++ scripts/background.js
@@ -0,0 +1,37 @@
+const URL = 'https://4tg9dbc6q8.execute-api.ap-south-1.amazonaws.com/production/prices';
+const REFRESH_INTERVAL = 30 * 1000;
+const VIEW_REFRESH_INTERVAL = 5 * 1000;
+
+// let messageSender = null;
+
+const fetchData = function() {
+  fetch(URL)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      if (data && data !== 'null') {
+        chrome.storage.local.set({ prices: data });
+      }
+      setTimeout(fetchData, REFRESH_INTERVAL);
+    });
+};
+
+setTimeout(fetchData, 0);
+
+// chrome.extension.onConnect.addListener(popup => {
+//   console.log('Popup connected', popup);
+//   if (popup.name === 'PricesCommunicator') {
+//     popup.onMessage.addListener(data => {
+//       console.log('Popup oonmessage', data);
+//       popup.postMessage(latestData);
+//     });
+//     console.log('Sending data', latestData);
+
+//     console.log('Message sent');
+//     messageSender = setInterval(() => popup.postMessage(latestData), VIEW_REFRESH_INTERVAL);
+//   }
+//   popup.onDisconnect.addListener(() => {
+//     clearInterval(messageSender);
+//     console.log('Popup disconnected', popup);
+//   });
+// });
---
+++ scripts/popup.js
@@ -0,0 +1,59 @@
+const equalBuySellExchanges = ['koinex'];
+
+function refreshData() {
+  chrome.storage.local.get('prices', function(data) {
+    if (!chrome.runtime.error) {
+      console.log('Storage items', data);
+      updateLayout(data.prices);
+
+      // setTimeout(refreshData, VIEW_REFRESH_INTERVAL);
+    }
+  });
+}
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+  for (key in changes) {
+    var storageChange = changes[key];
+
+    if (key === 'prices') {
+      updateLayout(storageChange.newValue);
+    }
+  }
+});
+
+refreshData();
+
+function updateLayout(prices) {
+  const exchangesNames = Object.keys(prices);
+
+  const lastUpdatedField = document.getElementById('last-updated-at');
+  if (lastUpdatedField) {
+    lastUpdatedField.innerHTML = `${new Date(prices.timestamp).toLocaleDateString()} ${new Date(
+      prices.timestamp,
+    ).toLocaleTimeString()}`;
+  } else {
+    return;
+  }
+
+  exchangesNames.forEach(exchangeName => {
+    const exchange = prices[exchangeName];
+
+    Object.keys(prices[exchangeName]).forEach(currencyName => {
+      const currency = exchange[currencyName];
+
+      const buyPrice = currency.buy;
+      const sellPrice = currency.sell;
+
+      const priceField = document.getElementById(`${exchangeName.toLowerCase()}-${currencyName.toLowerCase()}`);
+      if (priceField) {
+        if (equalBuySellExchanges.includes(exchangeName)) {
+          priceField.innerHTML = `<b>₹</b> ${buyPrice}`;
+        } else {
+          priceField.innerHTML = `Buy: <b>₹</b> ${buyPrice}<br />Sell: <b>₹</b> ${sellPrice}`;
+        }
+      } else {
+        console.log('Price field ', `${exchangeName.toLowerCase()}-${currencyName.toLowerCase()}`, 'does not exists');
+      }
+    });
+  });
+}
